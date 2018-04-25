905f3185bef67cdcbed8a5732cb9e5a8cfa6567c
hzf0799 <38243368+hzf0799@users.noreply.github.com>
2018-04-25 22:20:19 +0800

    Update vice.js

--- js/vice.js
+++ js/vice.js
@@ -23,6 +23,29 @@
         doc.write(wrap.innerHTML);
         wrap = null;
     }
+    //要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
+    refreshRem();
+
+    win.addEventListener("resize", function() {
+        clearTimeout(tid); //防止执行两次
+        tid = setTimeout(refreshRem, 300);
+    }, false);
+
+    win.addEventListener("pageshow", function(e) {
+        if (e.persisted) { // 浏览器后退的时候重新计算
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 300);
+        }
+    }, false);
+
+    if (doc.readyState === "complete") {
+        doc.body.style.fontSize = "16px";
+    } else {
+        doc.addEventListener("DOMContentLoaded", function(e) {
+            doc.body.style.fontSize = "16px";
+        }, false);
+    }
+})(375,1366);
 //标签页
 function openCity(evt,cityName){
     return function(){
