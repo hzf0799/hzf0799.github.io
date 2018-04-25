102f7cc86ea8c4ff9927bfefb4bab3b0b820f805
hzf0799 <38243368+hzf0799@users.noreply.github.com>
2018-04-25 22:19:07 +0800

    Update vice.js

--- js/vice.js
+++ js/vice.js
@@ -1,23 +1,4 @@
 
-
-function browserRedirect(){
-                var sUserAgent = navigator.userAgent.toLowerCase();
-                var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-                var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-                var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-                var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-                var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-                var bIsAndroid = sUserAgent.match(/android/i) == "android";
-                var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-                var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-                if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
-                  alret("ydd");
-                } else {
-                    //跳转pc端页面
-                    window.location.href="https://hzf0799.github.io/index.html";
-                }
-            }
-browserRedirect();
 //自适应
 (function(designWidth, maxWidth) {
     var doc = document,
